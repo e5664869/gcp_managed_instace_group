d473efa3aac5c69ab65954b25fbf88dbe8a48ede
Ganesh Mete <122775400+e5664869@users.noreply.github.com>
2024-03-17 18:08:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "gcp-project" {
+  type = string
+}
+variable "provider-region" {
+  type    = string
+  default = "us-central1"
+}
+variable "mig_vpc_name" {
+  type = string
+}
+variable "mig_subnet_name" {
+  type = string
+}
+variable "mig_subnet_region" {
+  type = list(string)
+}
+variable "mig_subnet_cidir_range" {
+  type = list(string)
+}
+variable "firewall_name" {
+  type = string
+}
+variable "firewall_name2" {
+  type = string
+}
+################################## Instance Template ##############
+variable "template_name" {
+  type = string
+}
+variable "template_region" {
+  type = string
+}
+variable "template_machine_type" {
+  type = string
+}
+variable "template_label" {
+  type = string
+}
+variable "template_metadata" {
+  type = map
+}
+################################## Load Balancer ##############
+variable "backend-svc-name" {
+  type = string
+}
+variable "backed-protocol" {
+  type = string
+}
